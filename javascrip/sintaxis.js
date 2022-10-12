93e26935d06dbc4d27e58eb54a02c897988c9a30
LINA SINIVA <linasiniva@gmail.com>
2022-10-11 20:51:29 -0500

    Version 1.0
    Version 1.0

---
+++ javascrip/sintaxis.js
@@ -0,0 +1,38 @@
+/*console.log(4 + 7) //11 //
+console.log(4 + "7") //47 //
+console.log("hola mundo")
+console.log(4 * "7") //28 //*/
+
+//Datos
+num = 40
+typeof num
+console.log(num)
+console.log(typeof(num)) //tipo de dato number
+nombre = "lina siniva"
+console.log(nombre)
+console.log(typeof(nombre)) //tipo de dato string
+booleano = true
+console.log(booleano)
+console.log(typeof(booleano)) //tipo de dato
+nulo = null
+console.log(null)
+console.log(typeof(null)) //tipo de dato tipo objeto
+
+console.log(typeof lina) // tipo de dato indefinido
+
+vector = []
+console.log(typeof vector)
+
+///variables /////
+
+var edad = 18
+let estatura = 1.53
+
+console.log("edad " + edad)
+console.log(estatura)
+
+let tecnologias = ["python", "java", "C#", "html5", "CSS3", "javascript"]
+console.log(tecnologias[2])
+console.log(tecnologias)
+
+let persona = {}
